9aa8d3f6fbfdb7503e3fc4153764cda6637c8359
Vicente <vicentek2021@gmail.com>
2021-12-14 14:34:18 -0500

    Managing Posts

--- APIdev/main.py
+++ APIdev/main.py
@@ -1,8 +1,33 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel  # Use this module to create an schema
+
 
 app = FastAPI()  # call the module
 
 
-@app.get("/")  # decorator
+class Post(BaseModel):  # create a class that automatically extracts the data from the posts
+    Tittle: str
+    Content: str
+    Published: bool = True  # if we define a default value then the field becomes optional
+    # this is another way to make a field optional and null, Need to import Optional module
+    Rating: Optional[int] = None
+
+
+@app.get("/")  # decorator aplies the fast api to the function with an http method
 def root():  # Mainfunction
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API this is a reload test"}
+
+
+@app.get("/post")
+def get_posts():
+    return {"data": "This is one post"}
+
+
+@app.post("/post/create")
+# Store the body of the post in an instance of the Post class
+def create_post(new_post: Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"new Post": f"Tittle: {new_post.Tittle}" + " " + f"Content: {new_post.Content}"}
--- APIdev/readme.txt
+++ APIdev/readme.txt
@@ -1,3 +1,11 @@
 1- Add a virtual enviroment with the modules needed
    Path> py -3 -m venv <name>
+   pip install fastapi[all]
+2- Add my first function to call an http get method to show a message in localhost
+   See Http methods (https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
+3- Start a live server with a reload option  $ uvicorn main: app --reload
+4- Install postman to automatically create a GUI to test and deploy our APIs
+5- Add a post request that takes input user data, saves it as a dictionary and display it in the form of a post
+6- Use Pydantic to create an schema, The post data can be easily accessed inheriting from the BaseModel class
+7- Create the Post Fields, required and Optionals 
    
